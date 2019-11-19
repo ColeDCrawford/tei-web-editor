1f6ab86958fd9d21efc660b2d224048fd1dfe063
coledcrawford <cole_crawford@fas.harvard.edu>
2019-11-19 23:23:15 +0000

    Updating resources and manifests to use HTTPS links

--- dist/mirador-instance.js
+++ dist/mirador-instance.js
@@ -11,17 +11,17 @@ var mir = Mirador({
           //"buildPath": "mirador-2.4/",
           'buildPath': 'mirador-2.7/',
 	  "data" : [
-	      {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/Latin-A"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/Latin-B"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/Latin-C"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/B1"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/B2"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/D"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/G"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/K1"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/K2"},
-              {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/N"},
-	      {"manifestUri": "http://digital-editing.fas.harvard.edu/manifests/S"}
+	      {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/Latin-A"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/Latin-B"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/Latin-C"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/B1"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/B2"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/D"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/G"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/K1"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/K2"},
+              {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/N"},
+	      {"manifestUri": "https://digital-editing.fas.harvard.edu/manifests/S"}
           ],
     'windowSettings' : {
 	"availableViews" : ['ThumbnailsView', 'ImageView'],
